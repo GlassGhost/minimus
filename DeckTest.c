b616762be483f59eec47eb26eb731b1e6850ae0e
GlassGhost <roypfund@gmail.com>
2023-12-20 14:36:01 -0600

    Signed-off-by: GlassGhost <roypfund@gmail.com>

---
+++ DeckTest.c
@@ -0,0 +1,114 @@
+#!/run/current-system/sw/bin/tcc -run
+ /*
+;* ^^^^^^^^^ comment the above lines for the scheme version ^^^^^^^^^^
+;*//* _________________________________________________________LICENSE
+;*    Copyright (c) 2023 Roy Pfund
+;*
+;* Licensed under the Apache License, Version 2.0 (the  "License");
+;* you may not use this file except in compliance with the License.
+;* You may obtain a copy of the License at
+;*
+;*     http://www.apache.org/licenses/LICENSE-2.0
+;*
+;* Unless required by applicable  law  or  agreed  to  in  writing,
+;* software distributed under the License is distributed on an  "AS
+;* IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,  either
+;* express or implied. See the License for  the  specific  language
+;* governing permissions and limitations under the License.
+;*//* ______________________________________________Standard libraries
+;* for zalloc() assert.h stdlib.h stdarg.h err.h*/ #include "zalloc.h" 
+/* 8-bits  16-bits  32-bits   64-bits */ #include "float_t.h" /*
+;* int8_t uint16_t uint32_t  uint64_t  // includes stdint.h
+;* int8_t  int16_t  int32_t   int64_t  // math.h inttypes.h
+;* XXXXXX  XXXXXX  float32_t float64_t // stdio.h and gives
+;* functions print_f64(x) print_f64p(&x) print_f32(x) print_f32p(&x)
+;*//* ________________________________________________________________
+build, run, and destroy with:
+gcc -m64 -O2 -std=gnu99 test.c -o test && ./test && rm ./test
+
+implements a circular deque with pushStart, popStart, pushEnd, and 
+popEnd operations. The deque is circularly arranged in the array data, 
+and the offsets are adjusted accordingly.
+;*//* ____________________________________________________#includes */
+
+
+#include "COA.h"
+DEFINE_DECK(float64_t)
+// #undef DEFINE_DECK
+
+
+/* ___________________________________________makeheaders INTERFACE */
+// makeheaders requires INTERFACE directive wrapped around Pre-Defined 
+// Constants & Data Structures accessible in other scopes.
+// Declarations provide the interface and the definitions are the
+// implementation.
+// #include "test.h" // ./makeheaders -local ./test.c
+#if INTERFACE
+/* ___________________________________________Pre-Defined Constants */
+
+/* _________________________________________________Data Structures */
+
+#endif // EXPORT_INTERFACE
+/* ________________________________________Function Implementations */
+float64_t average( Deck_float64_t* pile ) {
+    float64_t sum = 0.0;
+    float64_t num = pile->size;
+
+    // for(LLNode* tmp=pile->Start; tmp!=NULL; tmp=tmp->next){
+    for(LLNode_float64_t* tmp=pile->Start; tmp!=NULL; tmp=tmp->next){
+        sum += tmp->data;
+    }// linkedlist iteration
+    return sum/num;
+}
+
+float64_t pop_average( Deck_float64_t* pile ) {
+    // LLNode* temp = start;
+    float64_t sum = 0.0;
+    float64_t num = pile->size;
+    float64_t* f64bufptr = zalloc(sizeof(float64_t));
+    while(pile->size > 0){
+        popDeckStart_float64_t(f64bufptr, pile);
+        sum += *f64bufptr;
+    }
+    return sum/num;
+}
+
+/* __________________________________________Helper Implementations */
+// static functions are unavailable outside the file they are defined.
+static void test(int argc, char **argv){
+    Deck_float64_t* pile = zalloc(sizeof(Deck_float64_t));
+    float64_t* f64bufptr = zalloc(sizeof(float64_t));
+    * f64bufptr = 9999.2;
+    pushDeckStart_float64_t(f64bufptr, pile);
+    popDeckStart_float64_t(f64bufptr, pile);
+    print_f64(*f64bufptr);
+    // printf("size of pile is %"PRIu64" \n ", pile->size);
+    * f64bufptr = 2.0;
+    pushDeckStart_float64_t(f64bufptr, pile);
+    * f64bufptr = 3.0;
+    pushDeckStart_float64_t(f64bufptr, pile);
+    * f64bufptr = 4.0;
+    pushDeckStart_float64_t(f64bufptr, pile);
+    * f64bufptr = 5.0;
+    pushDeckStart_float64_t(f64bufptr, pile);
+
+    printf("\nsize of pile is %"PRIu64" \n ", pile->size);
+
+    printf("Average of 2, 3, 4, 5 = ");
+    print_f64(average(pile));
+    printf("\n");
+    printf("size of pile is %"PRIu64" \n ", pile->size);
+    
+    printf("Average of 2, 3, 4, 5 = ");
+    print_f64(pop_average(pile));
+    printf("\n");
+    printf("size of pile is %"PRIu64" \n ", pile->size);
+}
+/* _____________________________________________________RUN PROGRAM */
+
+int64_t main(int argc, char **argv){// *argv++ is *((char **)(argv++))
+    // https://stackoverflow.com/a/72203145/144020
+    // argv++; while (*argv) printf("%s ", *argv++); argv = argv - argc;
+    test( argc, argv);
+    return 0;
+}
---
+++ DequeArray.c
@@ -0,0 +1,73 @@
+#!/run/current-system/sw/bin/tcc -run
+ /*
+;* ^^^^^^^^^ comment the above lines for the scheme version ^^^^^^^^^^
+;*//* _________________________________________________________LICENSE
+;*    Copyright (c) 2023 Roy Pfund
+;*
+;* Licensed under the Apache License, Version 2.0 (the  "License");
+;* you may not use this file except in compliance with the License.
+;* You may obtain a copy of the License at
+;*
+;*     http://www.apache.org/licenses/LICENSE-2.0
+;*
+;* Unless required by applicable  law  or  agreed  to  in  writing,
+;* software distributed under the License is distributed on an  "AS
+;* IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,  either
+;* express or implied. See the License for  the  specific  language
+;* governing permissions and limitations under the License.
+;*//* ______________________________________________Standard libraries
+;* for zalloc() assert.h stdlib.h stdarg.h err.h*/ #include "zalloc.h" 
+/* 8-bits  16-bits  32-bits   64-bits */ #include "float_t.h" /*
+;* int8_t uint16_t uint32_t  uint64_t  // includes stdint.h
+;* int8_t  int16_t  int32_t   int64_t  // math.h inttypes.h
+;* XXXXXX  XXXXXX  float32_t float64_t // stdio.h and gives
+;* functions print_f64(x) print_f64p(&x) print_f32(x) print_f32p(&x)
+;*//* ________________________________________________________________
+build, run, and destroy with:
+gcc -m64 -O2 -std=gnu99 DequeArray.c -o DequeArray && ./DequeArray && rm ./DequeArray
+
+implements a circular deque with pushStart, popStart, pushEnd, and 
+popEnd operations. The deque is circularly arranged in the array data, 
+and the offsets are adjusted accordingly.
+;*//* ____________________________________________________#includes */
+
+
+/* ___________________________________________makeheaders INTERFACE */
+// makeheaders requires INTERFACE directive wrapped around Pre-Defined 
+// Constants & Data Structures accessible in other scopes.
+// Declarations provide the interface and the definitions are the
+// implementation.
+// #include "DequeArray.h" // ./makeheaders -local ./DequeArray.c
+#if INTERFACE
+/* ___________________________________________Pre-Defined Constants */
+
+#endif
+
+#include "COA.h"
+DEFINE_CIRCARRAY(uint8_t, 95) //(152*5)/8 // 4x content to metadata for 64 bit addresses
+// DEFINE_COA(uint8_t, 95) //(152*5)/8 // 4x content to metadata for 64 bit addresses
+// #undef DEFINE_DECK
+
+/* __________________________________________Helper Implementations */
+// static functions are unavailable outside the file they are defined.
+/* _____________________________________________________RUN PROGRAM */
+int64_t main(int argc, char **argv){// *argv++ is *((char **)(argv++))
+    CA_uint8_t* deque = zalloc(sizeof( CA_uint8_t ));
+    uint8_t* intbufptr = (uint8_t*)zalloc(sizeof(uint8_t));
+    // Pushing values onto the deque
+    *intbufptr = 42;
+    CA_pushStart_uint8_t(intbufptr, deque);
+    *intbufptr = 17;
+    CA_pushEnd_uint8_t(intbufptr, deque);
+    *intbufptr = 33;
+    CA_pushStart_uint8_t(intbufptr, deque);
+
+    // Popping values from the deque
+    CA_popStart_uint8_t(intbufptr, deque);
+    printf("Popped from start: %u\n", *intbufptr);
+    CA_popEnd_uint8_t(intbufptr, deque);
+    printf("Popped from end: %u\n", *intbufptr);
+    CA_popEnd_uint8_t(intbufptr, deque);
+    printf("Popped from end: %u\n", *intbufptr);
+    return 0;
+}
